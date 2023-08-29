85446cd1201e38961b70304f88a9789acbaf3976
VampireChicken12 <Johnbrown51@yahoo.com>
2023-08-29 04:54:21 -0400

    🚀 chore(release.yml): refactor release workflow and configuration 🔧 fix(release.config.cjs): update assets path in semantic-release configuration
    The release workflow has been refactored to separate the build and release steps into two separate jobs. The "build" job is responsible for building the extension, while the "release" job handles the actual release process. This separation allows for better organization and flexibility in the workflow.
    
    In the release configuration file, the assets path in the "@semantic-release/git" plugin has been updated to include the path to the generated extension ZIP file. This ensures that the ZIP file is included as part of the release assets, along with the other specified files.

--- release.config.cjs
+++ release.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
 		[
 			"@semantic-release/git",
 			{
-				assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+				assets: ["releases/youtube-enhancer-v${nextRelease.version}.zip", "CHANGELOG.md", "package.json", "package-lock.json"],
 				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
 			}
 		]
