eb6b239f249fa5afe3179a0a070f9ddf57bffbf5
VampireChicken12 <Johnbrown51@yahoo.com>
2023-08-29 01:45:53 -0400

    📦 chore(release.yml): add release workflow 🔧 chore(release.config.cjs): configure semantic-release for Git commits and versioning
    The release workflow file `release.yml` is added to the `.github/workflows` directory. This workflow is triggered on pushes to the `main` branch. It sets up the Node.js environment, installs dependencies, and runs Semantic Release to automate the release process.
    
    The `release.config.cjs` file is modified to include the `@semantic-release/git` plugin. This plugin is configured to generate a changelog, update the version in `package.json`, and commit the changes to Git. The commit message includes the release version and release notes. This ensures that the release process is automated and consistent.

--- release.config.cjs
+++ release.config.cjs
@@ -3,7 +3,14 @@ module.exports = {
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
 		"@semantic-release/changelog",
-		"@semantic-release/github"
+		"@semantic-release/github",
+		[
+			"@semantic-release/git",
+			{
+				assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+			}
+		]
 	],
 	preset: "angular",
 	branches: ["main", "dev"]
