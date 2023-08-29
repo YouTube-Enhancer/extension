510b2fe4d8dc0f119121d7adeb963a2d9e82084b
VampireChicken12 <Johnbrown51@yahoo.com>
2023-08-29 05:03:27 -0400

    feat(upload release zip): hopefully this works

--- release.config.cjs
+++ release.config.cjs
@@ -3,11 +3,21 @@ module.exports = {
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
 		"@semantic-release/changelog",
-		"@semantic-release/github",
+		[
+			"@semantic-release/github",
+			{
+				assets: [
+					{
+						path: "releases/youtube-enhancer-v${nextRelease.version}.zip",
+						label: "Release ${nextRelease.version}"
+					}
+				]
+			}
+		],
 		[
 			"@semantic-release/git",
 			{
-				assets: ["releases/youtube-enhancer-v${nextRelease.version}.zip", "CHANGELOG.md", "package.json", "package-lock.json"],
+				assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
 				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
 			}
 		]
