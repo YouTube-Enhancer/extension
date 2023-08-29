a5e6ec8dffab25f872f03c935d4e0e5ade96c636
VampireChicken12 <Johnbrown51@yahoo.com>
2023-08-29 06:41:47 -0400

    feat(ci): Automatic release with prebuilt zip

---
+++ release.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		[
+			"@semantic-release/github",
+			{
+				assets: [
+					{
+						path: "releases/*.zip",
+						name: "youtube-enhancer-v${nextRelease.version}.zip"
+					}
+				]
+			}
+		],
+		[
+			"@semantic-release/git",
+			{
+				assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+			}
+		]
+	],
+	preset: "angular",
+	branches: ["main", "dev"]
+};
