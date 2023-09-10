0458e45a51118ccf747237bcef88af464574a0c1
VampireChicken12 <Johnbrown51@yahoo.com>
2023-09-10 18:14:19 -0400

    feat: Add support for more browsers

---
+++ src/utils/plugins/copy-build.ts
@@ -0,0 +1,50 @@
+import * as fs from "fs";
+import * as path from "path";
+import colorLog from "../log";
+import { GetInstalledBrowsers } from "get-installed-browsers";
+import { PluginOption } from "vite";
+import { existsSync, mkdirSync, readdirSync, statSync, copyFileSync } from "fs";
+import { join } from "path";
+import { outputFolderName } from "../constants";
+function copyDirectorySync(sourceDir: string, targetDir: string) {
+	// Create the target directory if it doesn't exist
+	if (!existsSync(targetDir)) {
+		mkdirSync(targetDir, { recursive: true });
+	}
+
+	// Get a list of all files and subdirectories in the source directory
+	const items = readdirSync(sourceDir);
+
+	for (const item of items) {
+		const sourcePath = join(sourceDir, item);
+		const targetPath = join(targetDir, item);
+
+		// Check if the current item is a directory
+		if (statSync(sourcePath).isDirectory()) {
+			// Recursively copy the subdirectory
+			copyDirectorySync(sourcePath, targetPath);
+		} else {
+			// Copy the file
+			copyFileSync(sourcePath, targetPath);
+		}
+	}
+}
+const { resolve } = path;
+
+const outDir = resolve(__dirname, "..", "..", "..", outputFolderName);
+export default function copyBuild(): PluginOption {
+	return {
+		name: "copy-build",
+		closeBundle() {
+			const browsers = GetInstalledBrowsers();
+			if (!fs.existsSync(outDir)) {
+				fs.mkdirSync(outDir);
+			}
+			for (const browser of browsers) {
+				copyDirectorySync(resolve(outDir, "temp"), resolve(outDir, browser.name));
+				colorLog(`Build copy complete: ${resolve(outDir, browser.name)}`, "success");
+			}
+			fs.rmSync(resolve(outDir, "temp"), { recursive: true });
+		}
+	};
+}
---
+++ src/utils/plugins/make-release-zips.ts
@@ -0,0 +1,45 @@
+import * as fs from "fs";
+import * as path from "path";
+import colorLog from "../log";
+import pkg from "../../../package.json";
+import { PluginOption } from "vite";
+import archiver from "archiver";
+import { outputFolderName } from "../constants";
+import { GetInstalledBrowsers } from "get-installed-browsers";
+
+const { resolve } = path;
+
+const outDir = resolve(__dirname, "..", "..", "..", outputFolderName);
+const releaseDir = resolve(__dirname, "..", "..", "..", "releases");
+
+export default function makeReleaseZips(): PluginOption {
+	return {
+		name: "make-release-zips",
+
+		closeBundle() {
+			if (!fs.existsSync(releaseDir)) {
+				fs.mkdirSync(releaseDir);
+			}
+			const browsers = GetInstalledBrowsers();
+			for (const browser of browsers) {
+				if (!fs.existsSync(resolve(releaseDir, browser.name))) {
+					fs.mkdirSync(resolve(releaseDir, browser.name));
+				}
+				const releaseZipPath = resolve(releaseDir, browser.name, `${pkg.name}-v${pkg.version}-${browser.name}.zip`);
+				const releaseZipStream = fs.createWriteStream(releaseZipPath);
+				const releaseZip = archiver("zip", {
+					zlib: {
+						level: 9
+					}
+				});
+				releaseZip.pipe(releaseZipStream);
+				releaseZip.directory(resolve(outDir, browser.name), false);
+
+				releaseZipStream.on("close", () => {
+					colorLog(`Release zip file created: ${releaseZipPath}`, "success");
+				});
+				releaseZip.finalize();
+			}
+		}
+	};
+}
