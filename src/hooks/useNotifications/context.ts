d4f72b8b8b0532daaf8b403ac2d85849684af40b
VampireChicken12 <Johnbrown51@yahoo.com>
2023-11-12 09:37:19 -0500

    refactor: notifications context

---
+++ src/hooks/useNotifications/context.ts
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+import type { NotificationType, NotificationAction, Notification } from "@/src/@types";
+
+export type NotificationsContextProps = {
+	notifications: Notification[];
+	addNotification: (type: NotificationType, message: string, action?: NotificationAction) => void;
+	removeNotification: (notification: Notification) => void;
+};
+export const NotificationsContext = createContext<NotificationsContextProps | undefined>(undefined);
