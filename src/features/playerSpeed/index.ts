7e2ac3327e0ee5b58cc68dfc6fc8b96427f7bf06
VampireChicken12 <Johnbrown51@yahoo.com>
2023-12-15 13:25:23 -0500

    refactor: add generics to querySelector

--- src/features/playerSpeed/index.ts
+++ src/features/playerSpeed/index.ts
@@ -86,7 +86,7 @@ export function restorePlayerSpeed() {
 	video.playbackRate = Number(playerSpeed);
 }
 export function setupPlaybackSpeedChangeListener() {
-	const settingsPanelMenu = document.querySelector("div.ytp-settings-menu:not(#yte-feature-menu)");
+	const settingsPanelMenu = document.querySelector<HTMLDivElement>("div.ytp-settings-menu:not(#yte-feature-menu)");
 	const speedMenuItemClickListener = (event: Event) => {
 		const { target: speedMenuItem } = event as Event & { target: HTMLDivElement };
 		if (!speedMenuItem) return;
@@ -110,12 +110,12 @@ export function setupPlaybackSpeedChangeListener() {
 			const { target: targetElement } = mutation as MutationRecord & { target: HTMLDivElement };
 
 			// Check if the target element has the desired structure
-			const panelHeader: HTMLDivElement | null = targetElement.querySelector("div.ytp-panel > div.ytp-panel-header");
-			const panelMenu: HTMLDivElement | null = targetElement.querySelector("div.ytp-panel > div.ytp-panel-menu");
-			const menuItems = panelMenu?.querySelectorAll("div.ytp-menuitem");
+			const panelHeader = targetElement.querySelector<HTMLDivElement>("div.ytp-panel > div.ytp-panel-header");
+			const panelMenu = targetElement.querySelector<HTMLDivElement>("div.ytp-panel > div.ytp-panel-menu");
+			const menuItems = panelMenu?.querySelectorAll<HTMLDivElement>("div.ytp-menuitem");
 			if (panelHeader && panelMenu && menuItems && menuItems.length === 8) {
 				menuItems.forEach((menuItem) => {
-					eventManager.addEventListener(menuItem as HTMLDivElement, "click", speedMenuItemClickListener, "playerSpeed");
+					eventManager.addEventListener(menuItem, "click", speedMenuItemClickListener, "playerSpeed");
 				});
 			}
 		});
@@ -140,12 +140,14 @@ export function setupPlaybackSpeedChangeListener() {
 			childList: true,
 			subtree: true
 		});
-		const menuItems = [...document.querySelectorAll('div.ytp-settings-menu:not(#yte-feature-menu) > .ytp-panel > .ytp-panel-menu [role="menuitem"]')];
+		const menuItems = [
+			...document.querySelectorAll<HTMLDivElement>('div.ytp-settings-menu:not(#yte-feature-menu) > .ytp-panel > .ytp-panel-menu [role="menuitem"]')
+		];
 		const speedMenuItem = menuItems.find(
 			(el) =>
 				el.children[0].innerHTML ===
 				`<svg height="24" viewBox="0 0 24 24" width="24"><path d="M10,8v8l6-4L10,8L10,8z M6.3,5L5.7,4.2C7.2,3,9,2.2,11,2l0.1,1C9.3,3.2,7.7,3.9,6.3,5z            M5,6.3L4.2,5.7C3,7.2,2.2,9,2,11 l1,.1C3.2,9.3,3.9,7.7,5,6.3z            M5,17.7c-1.1-1.4-1.8-3.1-2-4.8L2,13c0.2,2,1,3.8,2.2,5.4L5,17.7z            M11.1,21c-1.8-0.2-3.4-0.9-4.8-2 l-0.6,.8C7.2,21,9,21.8,11,22L11.1,21z            M22,12c0-5.2-3.9-9.4-9-10l-0.1,1c4.6,.5,8.1,4.3,8.1,9s-3.5,8.5-8.1,9l0.1,1 C18.2,21.5,22,17.2,22,12z" fill="white"></path></svg>`
-		) as HTMLDivElement | undefined;
+		);
 		if (!speedMenuItem) return;
 		const {
 			children: [, , speedMenuItemContent]
