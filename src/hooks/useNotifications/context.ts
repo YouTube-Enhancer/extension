1049293497e11bc3e695356eb8ce983c54831fd5
VampireChicken12 <Johnbrown51@yahoo.com>
2023-12-21 00:23:22 -0500

    refactor: improve addNotification function use animation frame for notification progress bar

--- src/hooks/useNotifications/context.ts
+++ src/hooks/useNotifications/context.ts
@@ -5,7 +5,9 @@ import { createContext } from "react";
 export type AddNotification = (type: NotificationType, message: Notification["message"], action?: NotificationAction) => void;
 
 export type RemoveNotification = (notification: Notification) => void;
+export type CreateNotification = (type: NotificationType, message: Notification["message"], action?: NotificationAction) => Notification;
 
+export type ScheduleNotificationRemoval = (notification: Notification, removeAfterMs?: number) => void;
 export type NotificationsContextProps = {
 	addNotification: AddNotification;
 	notifications: Notification[];
