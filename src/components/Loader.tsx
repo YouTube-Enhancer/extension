e379caa8ff53132e07b8623350d8de1e9a9d6872
VampireChicken12 <Johnbrown51@yahoo.com>
2024-01-13 01:31:29 -0500

    refactor: add className to props

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,3 +1,12 @@
-export default function Loader() {
-	return <div className="m-auto h-12 w-12 animate-spin rounded-full border-[2.5px] border-solid border-[#181A1B] border-t-gray-700"></div>;
+import type { ClassValue } from "clsx";
+
+import { cn } from "../utils/utilities";
+
+type LoaderProps = {
+	className?: ClassValue;
+};
+export default function Loader({ className }: LoaderProps) {
+	return (
+		<div className={cn("m-auto h-12 w-12 animate-spin rounded-full border-[2.5px] border-solid border-[#181A1B] border-t-gray-700", className)}></div>
+	);
 }
