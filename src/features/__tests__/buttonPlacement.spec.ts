9f8573df4c0d8f70bf3dee7a325b182c09c52ae3
VampireChicken12 <Johnbrown51@yahoo.com>
2024-01-25 17:22:13 -0500

    Add button placement tests
    (cherry picked from commit cda88708dec413515d764e0c947d164b2bea094f)

--- src/features/__tests__/buttonPlacement.spec.ts
+++ src/features/__tests__/buttonPlacement.spec.ts
@@ -1,15 +1,38 @@
 import {
-	clickFeatureMenuItem,
+	clickFeatureButton,
 	disableFeature,
 	enableFeature,
 	expect,
-	expectFeatureMenuItemToBeFalsy,
-	expectFeatureMenuItemToBeTruthy,
+	expectFeatureButtonToBeFalsy,
+	expectFeatureButtonToBeIn,
+	expectFeatureButtonToBeTruthy,
 	navigateToOptionsPage,
 	navigateToYoutubePage,
+	selectOption,
 	test
 } from "playwright.config";
 test.beforeEach(async ({ extensionId, page }) => {
 	await navigateToOptionsPage(page, extensionId);
 });
 // TODO: code tests for button placements
+test("should place button in left controls", async ({ page }) => {
+	await selectOption(page, "button_placements.screenshotButton", "player_controls_left");
+	await enableFeature(page, "enable_screenshot_button");
+	await navigateToYoutubePage(page);
+	await expectFeatureButtonToBeTruthy(page, "yte-feature-screenshotButton-button");
+	await expectFeatureButtonToBeIn(page, "yte-feature-screenshotButton-button", "player_controls_left");
+});
+test("should place button in right controls", async ({ page }) => {
+	await selectOption(page, "button_placements.screenshotButton", "player_controls_right");
+	await enableFeature(page, "enable_screenshot_button");
+	await navigateToYoutubePage(page);
+	await expectFeatureButtonToBeTruthy(page, "yte-feature-screenshotButton-button");
+	await expectFeatureButtonToBeIn(page, "yte-feature-screenshotButton-button", "player_controls_right");
+});
+test("should place button below player", async ({ page }) => {
+	await selectOption(page, "button_placements.screenshotButton", "below_player");
+	await enableFeature(page, "enable_screenshot_button");
+	await navigateToYoutubePage(page);
+	await expectFeatureButtonToBeTruthy(page, "yte-feature-screenshotButton-button");
+	await expectFeatureButtonToBeIn(page, "yte-feature-screenshotButton-button", "below_player");
+});
