ce5f304d40b33d331bfd4986e90d68932a84de8e
VampireChicken12 <Johnbrown51@yahoo.com>
2024-03-13 14:32:01 -0400

    style(formatting): Run prettier update .prettierignore

--- src/features/playbackSpeedButtons/index.ts
+++ src/features/playbackSpeedButtons/index.ts
@@ -13,53 +13,47 @@ import type { AddButtonFunction, RemoveButtonFunction } from "../index";
 
 function playbackSpeedButtonClickListener(amount: number): () => void {
 	return () => {
-			void (async () => {
-					// Get the video element
-					const videoElement = document.querySelector<HTMLVideoElement>("video");
-					// If video element is not available, return
-					if (!videoElement) return;
-					try {
-						const { playbackRate: currentPlaybackSpeed } = videoElement;
-						const playerContainer =
-						isWatchPage() ? document.querySelector<YouTubePlayerDiv>("div#movie_player")
-						: isShortsPage() ? document.querySelector<YouTubePlayerDiv>("div#shorts-player")
-						: null;
-					// If player element is not available, return
-					if (!playerContainer) return;
-						const optionsData = await waitForSpecificMessage("options", "request_data", "content");
-						const {
-							data: {
-								options: {
-									osd_display_color,
-									osd_display_hide_time,
-									osd_display_opacity,
-									osd_display_padding,
-									osd_display_position,
-								}
-							}
-						} = optionsData;
-						new OnScreenDisplayManager(
-							{
-								displayColor: osd_display_color,
-								displayHideTime: osd_display_hide_time,
-								displayOpacity: osd_display_opacity,
-								displayPadding: osd_display_padding,
-								displayPosition: osd_display_position,
-								displayType: "text", // TODO: support for line/round? currently buggy
-								playerContainer: playerContainer
-							},
-							"yte-osd",
-							{
-								max: 4,
-								type: "speed",
-								value: currentPlaybackSpeed + amount
-							}
-						);
-						await setPlayerSpeed(currentPlaybackSpeed + amount);
-					} catch (error) {
-						console.error(error);
+		void (async () => {
+			// Get the video element
+			const videoElement = document.querySelector<HTMLVideoElement>("video");
+			// If video element is not available, return
+			if (!videoElement) return;
+			try {
+				const { playbackRate: currentPlaybackSpeed } = videoElement;
+				const playerContainer =
+					isWatchPage() ? document.querySelector<YouTubePlayerDiv>("div#movie_player")
+					: isShortsPage() ? document.querySelector<YouTubePlayerDiv>("div#shorts-player")
+					: null;
+				// If player element is not available, return
+				if (!playerContainer) return;
+				const optionsData = await waitForSpecificMessage("options", "request_data", "content");
+				const {
+					data: {
+						options: { osd_display_color, osd_display_hide_time, osd_display_opacity, osd_display_padding, osd_display_position }
 					}
-			})();
+				} = optionsData;
+				new OnScreenDisplayManager(
+					{
+						displayColor: osd_display_color,
+						displayHideTime: osd_display_hide_time,
+						displayOpacity: osd_display_opacity,
+						displayPadding: osd_display_padding,
+						displayPosition: osd_display_position,
+						displayType: "text", // TODO: support for line/round? currently buggy
+						playerContainer: playerContainer
+					},
+					"yte-osd",
+					{
+						max: 4,
+						type: "speed",
+						value: currentPlaybackSpeed + amount
+					}
+				);
+				await setPlayerSpeed(currentPlaybackSpeed + amount);
+			} catch (error) {
+				console.error(error);
+			}
+		})();
 	};
 }
 
@@ -79,7 +73,9 @@ export const addIncreasePlaybackSpeedButton: AddButtonFunction = async () => {
 	await addFeatureButton(
 		"increasePlaybackSpeedButton",
 		increasePlaybackSpeedButtonPlacement,
-		window.i18nextInstance.t("pages.content.features.playbackSpeedButtons.buttons.increasePlaybackSpeedButton.label", { SPEED: playbackSpeedPerClick }),
+		window.i18nextInstance.t("pages.content.features.playbackSpeedButtons.buttons.increasePlaybackSpeedButton.label", {
+			SPEED: playbackSpeedPerClick
+		}),
 		getFeatureIcon("increasePlaybackSpeedButton", increasePlaybackSpeedButtonPlacement !== "feature_menu" ? "shared_icon_position" : "feature_menu"),
 		playbackSpeedButtonClickListener(playbackSpeedPerClick),
 		false
@@ -102,7 +98,9 @@ export const addDecreasePlaybackSpeedButton: AddButtonFunction = async () => {
 	await addFeatureButton(
 		"decreasePlaybackSpeedButton",
 		decreasePlaybackSpeedButtonPlacement,
-		window.i18nextInstance.t("pages.content.features.playbackSpeedButtons.buttons.decreasePlaybackSpeedButton.label", { SPEED: playbackSpeedPerClick }),
+		window.i18nextInstance.t("pages.content.features.playbackSpeedButtons.buttons.decreasePlaybackSpeedButton.label", {
+			SPEED: playbackSpeedPerClick
+		}),
 		getFeatureIcon("decreasePlaybackSpeedButton", decreasePlaybackSpeedButtonPlacement !== "feature_menu" ? "shared_icon_position" : "feature_menu"),
 		playbackSpeedButtonClickListener(-playbackSpeedPerClick),
 		false
