e04a4f41a20f5fdbd284dc0b5b4585fccb104962
VampireChicken12 <Johnbrown51@yahoo.com>
2024-03-23 13:29:16 -0400

    fix: select not closing when clicking inside the labels container

--- src/components/Inputs/Select/Select.tsx
+++ src/components/Inputs/Select/Select.tsx
@@ -57,14 +57,9 @@ const Select = <Key extends configurationId>({
 
 	const disabledButtonClasses = { "dark:text-[#4b5563] text-[#4b5563]": disabled } satisfies ClassValue;
 	return (
-		<div
-			aria-valuetext={selectedOption}
-			className={cn("relative flex flex-row items-baseline justify-between gap-4", className)}
-			id={id}
-			ref={selectRef}
-		>
+		<div aria-valuetext={selectedOption} className={cn("relative flex flex-row items-baseline justify-between gap-4", className)} id={id}>
 			<label htmlFor={id}>{label}</label>
-			<div>
+			<div ref={selectRef}>
 				<>
 					<button
 						className={cn(
