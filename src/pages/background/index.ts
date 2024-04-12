5d5154547a7f3ef8fe692aa8af300e49830382bb
VampireChicken12 <Johnbrown51@yahoo.com>
2024-04-11 22:52:36 -0400

    chore: handle changing key values and removing keys

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,4 +1,7 @@
-import type { ContentToBackgroundSendOnlyMessageMappings } from "@/src/types";
+import type { ContentToBackgroundSendOnlyMessageMappings, configuration, configurationKeys } from "@/src/types";
+
+import { defaultConfiguration } from "@/src/utils/constants";
+import { parseStoredValue } from "@/src/utils/utilities";
 
 import pkg from "../../../package.json";
 
@@ -20,6 +23,7 @@ chrome.runtime.onInstalled.addListener((details) => {
 				// Open options page if a new major or minor version is released
 				void chrome.tabs.create({ url: "/src/pages/options/index.html" });
 			}
+			void updateStoredSettings();
 			break;
 		}
 	}
@@ -85,3 +89,52 @@ chrome.runtime.onMessage.addListener((message: ContentToBackgroundSendOnlyMessag
 		}
 	}
 });
+const changedKeys = Object.keys({
+	osd_display_type: ""
+} satisfies Partial<Record<configurationKeys, "">>);
+async function updateStoredSettings() {
+	try {
+		const settings = await getStoredSettings();
+		const removedKeys = Object.keys(settings).filter((key) => !Object.keys(defaultConfiguration).includes(key));
+		for (const key of removedKeys) {
+			delete settings[key];
+		}
+		for (const changedKey of changedKeys) {
+			switch (changedKey) {
+				case "osd_display_type": {
+					if ((settings.osd_display_type as unknown as string) === "round") {
+						settings.osd_display_type = "circle";
+					}
+					break;
+				}
+			}
+		}
+		await setModifiedSettings(settings);
+	} catch (error) {
+		console.error("Failed to update stored settings:", error);
+	}
+}
+async function setModifiedSettings(settings: Partial<configuration>) {
+	const updates: Record<string, string> = {};
+	for (const [key, value] of Object.entries(settings)) {
+		updates[key] = typeof value !== "string" ? JSON.stringify(value) : value;
+	}
+	await chrome.storage.local.set(updates);
+}
+async function getStoredSettings(): Promise<configuration> {
+	return new Promise((resolve, reject) => {
+		chrome.storage.local.get((settings) => {
+			try {
+				const storedSettings: Partial<configuration> = (
+					Object.keys(settings)
+						.filter((key) => typeof key === "string")
+						.filter((key) => Object.keys(defaultConfiguration).includes(key as unknown as string)) as configurationKeys[]
+				).reduce((acc, key) => Object.assign(acc, { [key]: parseStoredValue(settings[key] as string) }), {});
+				const castedSettings = storedSettings as configuration;
+				resolve(castedSettings);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	});
+}
