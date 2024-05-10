7865dfcdf6488229a988dc7217c26728785a7069
mist8kengas <mist8kengas1@gmail.com>
2024-05-10 14:10:22 +0400

    refactor: volume boost logic

--- src/features/volumeBoost/index.ts
+++ src/features/volumeBoost/index.ts
@@ -7,56 +7,48 @@ import eventManager from "@/src/utils/EventManager";
 import { browserColorLog, formatError, waitForSpecificMessage } from "@/src/utils/utilities";
 
 export default async function volumeBoost() {
-	const optionsData = await waitForSpecificMessage("options", "request_data", "content");
-
 	const {
 		data: {
 			options: { enable_volume_boost, volume_boost_amount, volume_boost_mode }
 		}
-	} = optionsData;
+	} = await waitForSpecificMessage("options", "request_data", "content");
 	if (!enable_volume_boost) return;
 	setupVolumeBoost();
 
-	if (volume_boost_mode === "per_video") {
-		await addVolumeBoostButton();
-	} else if (volume_boost_mode === "global") {
-		applyVolumeBoost(volume_boost_amount);
-	}
+	if (volume_boost_mode === "per_video") await addVolumeBoostButton();
+	else if (volume_boost_mode === "global") applyVolumeBoost(volume_boost_amount);
 }
 export async function enableVolumeBoost() {
 	setupVolumeBoost();
-	const optionsData = await waitForSpecificMessage("options", "request_data", "content");
 
 	const {
 		data: {
 			options: { volume_boost_amount }
 		}
-	} = optionsData;
+	} = await waitForSpecificMessage("options", "request_data", "content");
 
 	applyVolumeBoost(volume_boost_amount);
 }
 function setupVolumeBoost() {
-	if (!window.audioCtx || !window.gainNode) {
-		browserColorLog(`Enabling volume boost`, "FgMagenta");
-		try {
-			const player = document.querySelector<HTMLMediaElement>("video");
-			if (!player) return;
-			window.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-			const source = window.audioCtx.createMediaElementSource(player);
-			const gainNode = window.audioCtx.createGain();
-			source.connect(gainNode);
-			gainNode.connect(window.audioCtx.destination);
-			window.gainNode = gainNode;
-		} catch (error) {
-			browserColorLog(`Failed to enable volume boost: ${formatError(error)}`, "FgRed");
-		}
+	if (window.audioCtx && window.gainNode) return;
+	browserColorLog(`Enabling volume boost`, "FgMagenta");
+	try {
+		const player = document.querySelector<HTMLMediaElement>("video");
+		if (!player) return;
+		window.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+		const source = window.audioCtx.createMediaElementSource(player);
+		const gainNode = window.audioCtx.createGain();
+		source.connect(gainNode);
+		gainNode.connect(window.audioCtx.destination);
+		window.gainNode = gainNode;
+	} catch (error) {
+		browserColorLog(`Failed to enable volume boost: ${formatError(error)}`, "FgRed");
 	}
 }
 export function disableVolumeBoost() {
-	if (window.gainNode) {
-		browserColorLog(`Disabling volume boost`, "FgMagenta");
-		window.gainNode.gain.value = 1; // Set gain back to default
-	}
+	if (!window.gainNode) return;
+	browserColorLog(`Disabling volume boost`, "FgMagenta");
+	window.gainNode.gain.value = 1; // Set gain back to default
 }
 export function applyVolumeBoost(volume_boost_amount: number) {
 	browserColorLog(`Setting volume boost to ${Math.pow(10, volume_boost_amount / 20)}`, "FgMagenta");
@@ -64,15 +56,13 @@ export function applyVolumeBoost(volume_boost_amount: number) {
 	window.gainNode.gain.value = Math.pow(10, volume_boost_amount / 20);
 }
 export const addVolumeBoostButton: AddButtonFunction = async () => {
-	const optionsData = await waitForSpecificMessage("options", "request_data", "content");
-
 	const {
 		data: {
 			options: {
 				button_placements: { volumeBoostButton: volumeBoostButtonPlacement }
 			}
 		}
-	} = optionsData;
+	} = await waitForSpecificMessage("options", "request_data", "content");
 	await addFeatureButton(
 		"volumeBoostButton",
 		volumeBoostButtonPlacement,
@@ -82,17 +72,13 @@ export const addVolumeBoostButton: AddButtonFunction = async () => {
 		getFeatureIcon("volumeBoostButton", volumeBoostButtonPlacement !== "feature_menu" ? "shared_icon_position" : "feature_menu"),
 		(checked) => {
 			void (async () => {
-				if (checked !== undefined) {
-					updateFeatureButtonTitle(
-						"volumeBoostButton",
-						window.i18nextInstance.t(`pages.content.features.volumeBoostButton.button.toggle.${checked ? "on" : "off"}`)
-					);
-					if (checked) {
-						await enableVolumeBoost();
-					} else {
-						disableVolumeBoost();
-					}
-				}
+				if (checked === undefined) return;
+				updateFeatureButtonTitle(
+					"volumeBoostButton",
+					window.i18nextInstance.t(`pages.content.features.volumeBoostButton.button.toggle.${checked ? "on" : "off"}`)
+				);
+				if (checked) await enableVolumeBoost();
+				else disableVolumeBoost();
 			})();
 		},
 		true
