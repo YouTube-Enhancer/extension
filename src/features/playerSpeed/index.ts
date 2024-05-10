f75ca53cb3a2a6cee0895dab7e0a3fa115b559c2
mist8kengas <mist8kengas1@gmail.com>
2024-05-10 13:56:12 +0400

    refactor: player speed

--- src/features/playerSpeed/index.ts
+++ src/features/playerSpeed/index.ts
@@ -24,15 +24,13 @@ export async function setPlayerSpeed(input?: number): Promise<void> {
 	// If the input is a number, set the player speed to the given number
 	if (input === undefined) {
 		// Wait for the "options" message from the content script
-		const optionsData = await waitForSpecificMessage("options", "request_data", "content");
 		({
 			data: {
 				options: { enable_forced_playback_speed: enablePlayerSpeed, player_speed: playerSpeed }
 			}
-		} = optionsData);
-	} else if (typeof input === "number") {
-		playerSpeed = input;
-	}
+		} = await waitForSpecificMessage("options", "request_data", "content"));
+	} else playerSpeed = input;
+
 	// If the player speed is not specified, return
 	if (!playerSpeed) return;
 	// If forced playback speed option is disabled, return
@@ -95,9 +93,7 @@ export function setupPlaybackSpeedChangeListener() {
 		let playerSpeed: number = 1;
 		if (speedValueRegex.test(speedValue)) {
 			const speedValueMatch = speedValue.match(speedValueRegex);
-			if (speedValueMatch) {
-				playerSpeed = Number(speedValueMatch[1]);
-			}
+			if (speedValueMatch) playerSpeed = Number(speedValueMatch[1]);
 		}
 		window.localStorage.setItem("playerSpeed", String(playerSpeed));
 	};
@@ -130,31 +126,29 @@ export function setupPlaybackSpeedChangeListener() {
 	});
 	const config: MutationObserverInit = { childList: true, subtree: true };
 
-	if (settingsPanelMenu) {
-		playerSpeedMenuObserver.observe(settingsPanelMenu, config);
-		customSpeedSliderObserver.observe(settingsPanelMenu, {
-			attributeFilter: ["aria-valuenow"],
-			attributes: true,
-			childList: true,
-			subtree: true
-		});
-		const menuItems = [
-			...document.querySelectorAll<HTMLDivElement>('div.ytp-settings-menu:not(#yte-feature-menu) > .ytp-panel > .ytp-panel-menu [role="menuitem"]')
-		];
-		const speedMenuItem = menuItems.find(
-			(el) =>
-				el.children[0].innerHTML ===
-				`<svg height="24" viewBox="0 0 24 24" width="24"><path d="M10,8v8l6-4L10,8L10,8z M6.3,5L5.7,4.2C7.2,3,9,2.2,11,2l0.1,1C9.3,3.2,7.7,3.9,6.3,5z            M5,6.3L4.2,5.7C3,7.2,2.2,9,2,11 l1,.1C3.2,9.3,3.9,7.7,5,6.3z            M5,17.7c-1.1-1.4-1.8-3.1-2-4.8L2,13c0.2,2,1,3.8,2.2,5.4L5,17.7z            M11.1,21c-1.8-0.2-3.4-0.9-4.8-2 l-0.6,.8C7.2,21,9,21.8,11,22L11.1,21z            M22,12c0-5.2-3.9-9.4-9-10l-0.1,1c4.6,.5,8.1,4.3,8.1,9s-3.5,8.5-8.1,9l0.1,1 C18.2,21.5,22,17.2,22,12z" fill="white"></path></svg>`
-		);
-		if (!speedMenuItem) return;
-		const {
-			children: [, , speedMenuItemContent]
-		} = speedMenuItem;
-		if (!speedMenuItemContent) return;
-		const { textContent: speedValue } = speedMenuItem;
-		// If the playback speed is not available, return
-		if (!speedValue) return;
-		const playerSpeed = isNaN(Number(speedValue)) ? 1 : Number(speedValue);
-		window.localStorage.setItem("playerSpeed", String(playerSpeed));
-	}
+	if (!settingsPanelMenu) return;
+	playerSpeedMenuObserver.observe(settingsPanelMenu, config);
+	customSpeedSliderObserver.observe(settingsPanelMenu, {
+		attributeFilter: ["aria-valuenow"],
+		attributes: true,
+		childList: true,
+		subtree: true
+	});
+	const menuItems = Array.from(
+		document.querySelectorAll<HTMLDivElement>('div.ytp-settings-menu:not(#yte-feature-menu) > .ytp-panel > .ytp-panel-menu [role="menuitem"]')
+	);
+	const speedMenuItem = menuItems.find(
+		(el) =>
+			el.children[0].innerHTML ===
+			`<svg height="24" viewBox="0 0 24 24" width="24"><path d="M10,8v8l6-4L10,8L10,8z M6.3,5L5.7,4.2C7.2,3,9,2.2,11,2l0.1,1C9.3,3.2,7.7,3.9,6.3,5z            M5,6.3L4.2,5.7C3,7.2,2.2,9,2,11 l1,.1C3.2,9.3,3.9,7.7,5,6.3z            M5,17.7c-1.1-1.4-1.8-3.1-2-4.8L2,13c0.2,2,1,3.8,2.2,5.4L5,17.7z            M11.1,21c-1.8-0.2-3.4-0.9-4.8-2 l-0.6,.8C7.2,21,9,21.8,11,22L11.1,21z            M22,12c0-5.2-3.9-9.4-9-10l-0.1,1c4.6,.5,8.1,4.3,8.1,9s-3.5,8.5-8.1,9l0.1,1 C18.2,21.5,22,17.2,22,12z" fill="white"></path></svg>`
+	);
+	if (!speedMenuItem) return;
+	const {
+		children: [, , speedMenuItemContent],
+		textContent: speedValue
+	} = speedMenuItem;
+	// If the playback speed is not available, return
+	if (!speedMenuItemContent || !speedValue) return;
+	const playerSpeed = isNaN(Number(speedValue)) ? 1 : Number(speedValue);
+	window.localStorage.setItem("playerSpeed", String(playerSpeed));
 }
