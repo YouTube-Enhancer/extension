f6383ac3d1a2b02fc5215173023d02fea6c1111f
mist8kengas <mist8kengas1@gmail.com>
2024-05-10 14:11:53 +0400

    refactor: remember volume untested

--- src/features/rememberVolume/utils.ts
+++ src/features/rememberVolume/utils.ts
@@ -4,12 +4,11 @@ import eventManager from "@/src/utils/EventManager";
 import { browserColorLog, isShortsPage, isWatchPage, sendContentOnlyMessage, waitForSpecificMessage } from "@/src/utils/utilities";
 export async function setupVolumeChangeListener() {
 	// Wait for the "options" message from the content script
-	const optionsData = await waitForSpecificMessage("options", "request_data", "content");
 	const {
 		data: {
 			options: { enable_remember_last_volume: enableRememberVolume }
 		}
-	} = optionsData;
+	} = await waitForSpecificMessage("options", "request_data", "content");
 	// If the volume is not being remembered, return
 	if (!enableRememberVolume) return;
 	const IsWatchPage = isWatchPage();
@@ -29,13 +28,10 @@ export async function setupVolumeChangeListener() {
 			void (async () => {
 				if (!currentTarget) return;
 				const newVolume = await playerContainer.getVolume();
-				if (IsWatchPage) {
-					// Send a "setVolume" message to the content script
-					sendContentOnlyMessage("setRememberedVolume", { watchPageVolume: newVolume });
-				} else if (IsShortsPage) {
-					// Send a "setVolume" message to the content script
-					sendContentOnlyMessage("setRememberedVolume", { shortsPageVolume: newVolume });
-				}
+				// Send a "setVolume" message to the content script
+				if (IsWatchPage) sendContentOnlyMessage("setRememberedVolume", { watchPageVolume: newVolume });
+				// Send a "setVolume" message to the content script
+				else if (IsShortsPage) sendContentOnlyMessage("setRememberedVolume", { shortsPageVolume: newVolume });
 			})();
 		},
 		"rememberVolume"
@@ -55,16 +51,15 @@ export async function setRememberedVolume({
 	rememberedVolumes: configuration["remembered_volumes"];
 }) {
 	// If the remembered volume option is enabled, set the volume and draw the volume display
-	if (rememberedVolumes && enableRememberVolume) {
-		const { shortsPageVolume, watchPageVolume } = rememberedVolumes ?? {};
-		if (isWatchPage && watchPageVolume) {
-			// Log the message indicating whether the last volume is being restored or not
-			browserColorLog(`Restoring watch page volume to ${watchPageVolume}`, "FgMagenta");
-			await playerContainer.setVolume(watchPageVolume);
-		} else if (isShortsPage && shortsPageVolume) {
-			// Log the message indicating whether the last volume is being restored or not
-			browserColorLog(`Restoring shorts page volume to ${shortsPageVolume}`, "FgMagenta");
-			await playerContainer.setVolume(shortsPageVolume);
-		}
+	if (!rememberedVolumes || !enableRememberVolume) return;
+	const { shortsPageVolume, watchPageVolume } = rememberedVolumes ?? {};
+	if (isWatchPage && watchPageVolume) {
+		// Log the message indicating whether the last volume is being restored or not
+		browserColorLog(`Restoring watch page volume to ${watchPageVolume}`, "FgMagenta");
+		await playerContainer.setVolume(watchPageVolume);
+	} else if (isShortsPage && shortsPageVolume) {
+		// Log the message indicating whether the last volume is being restored or not
+		browserColorLog(`Restoring shorts page volume to ${shortsPageVolume}`, "FgMagenta");
+		await playerContainer.setVolume(shortsPageVolume);
 	}
 }
