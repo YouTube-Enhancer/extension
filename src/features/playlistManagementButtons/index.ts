b9ac842c2e42c0af0e44642bb050b945fd361030
VampireChicken12 <Johnbrown51@yahoo.com>
2025-10-09 03:11:38 -0400

    remove redundant code

--- src/features/playlistManagementButtons/index.ts
+++ src/features/playlistManagementButtons/index.ts
@@ -55,7 +55,7 @@ export async function enablePlaylistManagementButtons() {
 	function addButtonToPlaylistItems() {
 		const playlistItems = document.querySelectorAll(PLAYLIST_ITEM_SELECTOR);
 		playlistItems.forEach((item) => {
-			if (!item.querySelector("#index-container") || item.querySelector(".yte-remove-button") || item.querySelector(".yte-reset-button")) {
+			if (item.querySelector(".yte-remove-button") || item.querySelector(".yte-reset-button")) {
 				return;
 			}
 
