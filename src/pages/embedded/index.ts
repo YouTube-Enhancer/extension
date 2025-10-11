673bd449458a856c54417571d6a5624a0af057f7
VampireChicken12 <Johnbrown51@yahoo.com>
2025-10-11 17:17:52 -0400

    update translation context screenshots

--- src/pages/embedded/index.ts
+++ src/pages/embedded/index.ts
@@ -59,7 +59,6 @@ import {
 import setPlayerQuality from "@/src/features/playerQuality";
 import { restorePlayerSpeed, setPlayerSpeed, setupPlaybackSpeedChangeListener } from "@/src/features/playerSpeed";
 import { disablePlaylistLength, enablePlaylistLength } from "@/src/features/playlistLength";
-import { disablePlaylistManagementButtons, enablePlaylistManagementButtons } from "@/src/features/playlistManagementButtons";
 import { setupRemainingTime as enableRemainingTime, removeRemainingTimeDisplay } from "@/src/features/remainingTime";
 import enableRememberVolume from "@/src/features/rememberVolume";
 import enableRemoveRedirect from "@/src/features/removeRedirect";
@@ -266,8 +265,7 @@ const enableFeatures = async () => {
 			enableAutomaticallyEnableClosedCaptions(),
 			enableAutomaticallyDisableAmbientMode(),
 			enableDefaultToOriginalAudioTrack(),
-			enableRestoreFullscreenScrolling(),
-			enablePlaylistManagementButtons()
+			enableRestoreFullscreenScrolling()
 		]);
 	} finally {
 		isEnablingFeatures = false;
@@ -915,17 +913,6 @@ const initialize = function () {
 					case "playlistWatchTimeGetMethodChange": {
 						disablePlaylistLength();
 						await enablePlaylistLength();
-						break;
-					}
-					case "playlistManagementButtonsChange": {
-						const {
-							data: { playlistManagementButtonsEnabled }
-						} = message;
-						if (playlistManagementButtonsEnabled) {
-							await enablePlaylistManagementButtons();
-						} else {
-							await disablePlaylistManagementButtons();
-						}
 						break;
 					}
 					case "remainingTimeChange": {
