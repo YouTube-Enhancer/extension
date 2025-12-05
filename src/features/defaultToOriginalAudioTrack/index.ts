8284222836159ebb2e4bde6fea7beb08fe02b350
VampireChicken12 <Johnbrown51@yahoo.com>
2025-12-05 15:45:15 -0500

    fix(default to original audio track): More robust way of handling the audio tracks implemented

--- src/features/defaultToOriginalAudioTrack/index.ts
+++ src/features/defaultToOriginalAudioTrack/index.ts
@@ -3,7 +3,8 @@ import type { audioTrack } from "node_modules/@types/youtube-player/dist/types";
 import type { Nullable, YouTubePlayerDiv } from "@/src/types";
 
 import { isWatchPage, waitForElement, waitForSpecificMessage } from "@/src/utils/utilities";
-let originalAudioTrack: Nullable<audioTrack> = null;
+let originalAudioTrack: Nullable<ParsedAudioTrack> = null;
+type ParsedAudioTrack = { isDefault: boolean; name: string; track: audioTrack };
 /**
  * Reverts the audio track to the one that was selected by the user
  * when the feature was enabled.
@@ -15,11 +16,10 @@ export async function disableDefaultToOriginalAudioTrack() {
 	// If the original audio track is not stored, do nothing
 	if (!originalAudioTrack) return;
 	// Revert the audio track to the one that was selected by the user
-	await playerContainer.setAudioTrack(originalAudioTrack);
+	await playerContainer.setAudioTrack(originalAudioTrack.track);
 	// Reset the original audio track
 	originalAudioTrack = null;
 }
-
 /**
  * Enables the feature to default to the original audio track.
  */
@@ -34,13 +34,38 @@ export async function enableDefaultToOriginalAudioTrack() {
 	const playerContainer = await waitForElement<YouTubePlayerDiv>("div#movie_player");
 	if (!playerContainer) return;
 	const audioTracks = await playerContainer.getAvailableAudioTracks();
-	const defaultAudioTrack = audioTracks.find((track) => track.tq.name.toLowerCase().includes("original"));
+	const defaultAudioTrack = findDefaultTrack(audioTracks);
 	if (!defaultAudioTrack) return;
-	const currentAudioTrack = await playerContainer.getAudioTrack();
+	const currentAudioTrack = parseAudioTrack(await playerContainer.getAudioTrack());
+	if (!currentAudioTrack) return;
 	// Store the original audio track in the "originalAudioTrack" variable
 	if (!originalAudioTrack) originalAudioTrack = currentAudioTrack;
 	// If the current audio track is the same as the default audio track, do nothing
-	if (defaultAudioTrack.tq.name === currentAudioTrack.tq.name) return;
+	if (defaultAudioTrack.name === currentAudioTrack.name) return;
 	// Set the audio track to the default audio track
-	await playerContainer.setAudioTrack(defaultAudioTrack);
+	await playerContainer.setAudioTrack(defaultAudioTrack.track);
+}
+function findDefaultTrack(tracks: Record<string, unknown>[]): Nullable<ParsedAudioTrack> {
+	for (const track of tracks) {
+		const audio = parseAudioTrack(track);
+		if (audio && audio.isDefault === true) {
+			return audio;
+		}
+	}
+	return null;
+}
+
+function parseAudioTrack(obj: Record<string, unknown>): Nullable<ParsedAudioTrack> {
+	for (const key of Object.keys(obj)) {
+		const { [key]: value } = obj;
+		if (value && typeof value === "object" && !Array.isArray(value) && "name" in value && "isDefault" in value) {
+			return {
+				isDefault: value.isDefault as boolean,
+				name: value.name as string,
+				track: obj
+			};
+		}
+	}
+
+	return null;
 }
