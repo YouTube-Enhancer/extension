8d0a6521f2a3690b87751f5fa43389d8c114a85b
VampireChicken12 <Johnbrown51@yahoo.com>
2026-01-03 01:30:32 -0500

    refactor(hide paid promotion banner): Improve hiding logic by using nested selectors

--- src/features/hidePaidPromotionBanner/index.ts
+++ src/features/hidePaidPromotionBanner/index.ts
@@ -5,7 +5,7 @@ import "./index.css";
 export function disableHidePaidPromotionBanner() {
 	modifyElementClassList("remove", {
 		className: "yte-hide-paid-promotion-banner",
-		element: document.querySelector(".ytp-paid-content-overlay")
+		element: document.body
 	});
 }
 
@@ -18,6 +18,6 @@ export async function enableHidePaidPromotionBanner() {
 	if (!enable_hide_paid_promotion_banner) return;
 	modifyElementClassList("add", {
 		className: "yte-hide-paid-promotion-banner",
-		element: document.querySelector(".ytp-paid-content-overlay")
+		element: document.body
 	});
 }
