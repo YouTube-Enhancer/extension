f68f84c6553cfee0a06403aad0a3f86c11a048ca
VampireChicken12 <Johnbrown51@yahoo.com>
2026-01-03 01:30:55 -0500

    refactor(hide playables): Improve hiding logic by using nested selectors

--- src/features/hidePlayables/utils.ts
+++ src/features/hidePlayables/utils.ts
@@ -1,19 +1,14 @@
-import { modifyElementsClassList } from "@/src/utils/utilities";
+import { modifyElementClassList } from "@/src/utils/utilities";
 
-const playablesSelector = "ytd-rich-section-renderer:has(a[href='/playables'])";
 export async function hidePlayables() {
-	modifyElementsClassList("add", "yte-hide-playables", document.querySelectorAll(playablesSelector));
-}
-export function observePlayables() {
-	const observer = new MutationObserver(async (mutations) => {
-		const containsPlayables = mutations.some((mutation) => mutation.target instanceof Element && mutation.target.matches(playablesSelector));
-		if (containsPlayables) {
-			await hidePlayables();
-		}
+	modifyElementClassList("add", {
+		className: "yte-hide-playables",
+		element: document.body
 	});
-	observer.observe(document.body, { childList: true, subtree: true });
-	return observer;
 }
 export async function showPlayables() {
-	modifyElementsClassList("remove", "yte-hide-playables", document.querySelectorAll(playablesSelector));
+	modifyElementClassList("remove", {
+		className: "yte-hide-playables",
+		element: document.body
+	});
 }
