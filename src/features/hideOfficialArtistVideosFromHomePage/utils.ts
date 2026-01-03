1cbe0b9313ade88b8f8df743eba5778041faa27a
VampireChicken12 <Johnbrown51@yahoo.com>
2026-01-03 01:30:08 -0500

    refactor(hide official artist videos from home page): Improve hiding logic by using nested selectors

--- src/features/hideOfficialArtistVideosFromHomePage/utils.ts
+++ src/features/hideOfficialArtistVideosFromHomePage/utils.ts
@@ -1,27 +1,14 @@
-import { modifyElementsClassList } from "@/src/utils/utilities";
+import { modifyElementClassList } from "@/src/utils/utilities";
 
 export function hideOfficialArtistVideosFromHomePage() {
-	modifyElementsClassList(
-		"add",
-		"yte-hide-official-artist-videos-from-home-page",
-		document.querySelectorAll<HTMLElement>(
-			"ytd-rich-item-renderer:has(path[d='M19.222 9.008a3 3 0 010 5.984 3 3 0 01-4.23 4.232 3 3 0 01-5.984 0 3 3 0 01-4.23-4.232 3 3 0 010-5.984 3 3 0 014.23-4.231 3 3 0 015.984 0 3 3 0 014.23 4.231ZM12 7v5.5a2.5 2.5 0 101 2V10h3V7h-4Z'])"
-		)
-	);
-}
-export function observeOfficialArtistVideosFromHomePage() {
-	const observer = new MutationObserver(() => {
-		hideOfficialArtistVideosFromHomePage();
+	modifyElementClassList("add", {
+		className: "yte-hide-official-artist-videos-from-home-page",
+		element: document.body
 	});
-	observer.observe(document.body, { childList: true, subtree: true });
-	return observer;
 }
 export function showOfficialArtistVideosFromHomePage() {
-	modifyElementsClassList(
-		"remove",
-		"yte-hide-official-artist-videos-from-home-page",
-		document.querySelectorAll<HTMLElement>(
-			"ytd-rich-item-renderer:has(path[d='M19.222 9.008a3 3 0 010 5.984 3 3 0 01-4.23 4.232 3 3 0 01-5.984 0 3 3 0 01-4.23-4.232 3 3 0 010-5.984 3 3 0 014.23-4.231 3 3 0 015.984 0 3 3 0 014.23 4.231ZM12 7v5.5a2.5 2.5 0 101 2V10h3V7h-4Z'])"
-		)
-	);
+	modifyElementClassList("remove", {
+		className: "yte-hide-official-artist-videos-from-home-page",
+		element: document.body
+	});
 }
