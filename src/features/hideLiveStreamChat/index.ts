4849b5c03e747ed5797cd917dad5228d675afb2b
VampireChicken12 <Johnbrown51@yahoo.com>
2026-01-03 01:29:19 -0500

    refactor(hide live stream chat): Improve hiding logic by using nested selectors

--- src/features/hideLiveStreamChat/index.ts
+++ src/features/hideLiveStreamChat/index.ts
@@ -1,15 +1,17 @@
 import type { YouTubePlayerDiv } from "@/src/types";
 
-import { modifyElementsClassList, waitForElement, waitForSpecificMessage } from "@/src/utils/utilities";
+import { modifyElementClassList, waitForElement, waitForSpecificMessage } from "@/src/utils/utilities";
 
 import "./index.css";
-const hideLiveStreamChatSelectors = ["div#chat-container", "div#chat-container #chat", "#full-bleed-container #panels-full-bleed-container"];
 export async function disableHideLiveStreamChat() {
 	const player = await waitForElement<YouTubePlayerDiv>("div#movie_player");
 	if (!player) return;
 	const playerData = await player.getVideoData();
 	if (!playerData.isLive) return;
-	modifyElementsClassList("remove", "yte-hide-live-stream-chat", hideLiveStreamChatSelectors);
+	modifyElementClassList("remove", {
+		className: "yte-hide-live-stream-chat",
+		element: document.body
+	});
 }
 
 export async function enableHideLiveStreamChat() {
@@ -23,5 +25,8 @@ export async function enableHideLiveStreamChat() {
 	if (!player) return;
 	const playerData = await player.getVideoData();
 	if (!playerData.isLive) return;
-	modifyElementsClassList("add", "yte-hide-live-stream-chat", hideLiveStreamChatSelectors);
+	modifyElementClassList("add", {
+		className: "yte-hide-live-stream-chat",
+		element: document.body
+	});
 }
