b4ee8530111d712c5e594bea58e5aed2fafbcea1
VampireChicken12 <Johnbrown51@yahoo.com>
2026-01-03 01:30:08 -0500

    refactor(hide official artist videos from home page): Improve hiding logic by using nested selectors

--- src/features/hideOfficialArtistVideosFromHomePage/index.ts
+++ src/features/hideOfficialArtistVideosFromHomePage/index.ts
@@ -1,20 +1,12 @@
-import type { Nullable } from "@/src/types";
-
 import {
 	hideOfficialArtistVideosFromHomePage,
-	observeOfficialArtistVideosFromHomePage,
 	showOfficialArtistVideosFromHomePage
 } from "@/src/features/hideOfficialArtistVideosFromHomePage/utils";
 import { waitForSpecificMessage } from "@/src/utils/utilities";
 
 import "./index.css";
-let officialArtistVideosObserver: Nullable<MutationObserver> = null;
 export function disableHideOfficialArtistVideosFromHomePage() {
 	showOfficialArtistVideosFromHomePage();
-	if (officialArtistVideosObserver) {
-		officialArtistVideosObserver.disconnect();
-		officialArtistVideosObserver = null;
-	}
 }
 
 export async function enableHideOfficialArtistVideosFromHomePage() {
@@ -26,6 +18,4 @@ export async function enableHideOfficialArtistVideosFromHomePage() {
 	} = await waitForSpecificMessage("options", "request_data", "content");
 	if (!enableHideOfficialArtistVideosFromHomePage) return;
 	hideOfficialArtistVideosFromHomePage();
-	if (officialArtistVideosObserver) officialArtistVideosObserver.disconnect();
-	officialArtistVideosObserver = observeOfficialArtistVideosFromHomePage();
 }
