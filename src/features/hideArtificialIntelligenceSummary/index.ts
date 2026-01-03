9904457d97a66bfa409e314a039ed659f1448d65
VampireChicken12 <Johnbrown51@yahoo.com>
2026-01-03 01:28:00 -0500

    fix(hide artificial intelligence summary): not hiding summary

--- src/features/hideArtificialIntelligenceSummary/index.ts
+++ src/features/hideArtificialIntelligenceSummary/index.ts
@@ -1,21 +1,10 @@
 import "./index.css";
-import type { Nullable } from "@/src/types";
 
-import {
-	hideArtificialIntelligenceSummary,
-	observeArtificialIntelligenceSummary,
-	showArtificialIntelligenceSummary
-} from "@/src/features/hideArtificialIntelligenceSummary/utils";
+import { hideArtificialIntelligenceSummary, showArtificialIntelligenceSummary } from "@/src/features/hideArtificialIntelligenceSummary/utils";
 import { waitForSpecificMessage } from "@/src/utils/utilities";
 
-let aiSummaryObserver: Nullable<MutationObserver> = null;
 export async function disableHideArtificialIntelligenceSummary() {
 	await showArtificialIntelligenceSummary();
-	// Disconnect the observer
-	if (aiSummaryObserver) {
-		aiSummaryObserver.disconnect();
-		aiSummaryObserver = null;
-	}
 }
 export async function enableHideArtificialIntelligenceSummary() {
 	// Wait for the "options" message from the content script
@@ -26,6 +15,4 @@ export async function enableHideArtificialIntelligenceSummary() {
 	} = await waitForSpecificMessage("options", "request_data", "content");
 	if (!enableHideArtificialIntelligenceSummary) return;
 	await hideArtificialIntelligenceSummary();
-	if (aiSummaryObserver) aiSummaryObserver.disconnect();
-	aiSummaryObserver = await observeArtificialIntelligenceSummary();
 }
