657164e4389070a1a85c2248f161989e887de3cf
VampireChicken12 <Johnbrown51@yahoo.com>
2026-02-02 23:41:51 -0500

    chore(deep dark theme): bump DeepDark theme to v4.7.3

--- src/deepDarkPresets.ts
+++ src/deepDarkPresets.ts
@@ -3,6 +3,7 @@
  * Author: https://github.com/RaitaroH
  * Co-authors: https://github.com/MechaLynx
  * Repository: https://github.com/RaitaroH/YouTube-DeepDark
+ * Version: 4.7.3
  */
 export const deepDarkPreset = [
 	"9anime",
