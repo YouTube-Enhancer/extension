04a87b8200f10ffd898328db548ce118a0d758f3
VampireChicken12 <Johnbrown51@yahoo.com>
2026-02-03 21:22:12 -0500

    feat(Global Volume): Always set volume to the same volume based on user settings #1044

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -588,7 +588,9 @@ export default function Settings() {
 				<SettingSection title={t((translations) => translations.settings.sections.miscellaneous.title)}>
 					<SettingTitle />
 					<Setting
-						checked={settings.enable_remember_last_volume?.toString() === "true"}
+						checked={settings.enable_global_volume?.toString() !== "true" && settings.enable_remember_last_volume?.toString() === "true"}
+						disabled={settings.enable_global_volume?.toString() === "true"}
+						disabledReason={t((translations) => translations.pages.options.extras.optionDisabled.specificOption.rememberVolume)}
 						label={t((translations) => translations.settings.sections.miscellaneous.settings.rememberVolume.enable.label)}
 						onChange={setCheckboxOption("enable_remember_last_volume")}
 						parentSetting={null}
@@ -1420,6 +1422,30 @@ export default function Settings() {
 							/>
 						</>
 					)}
+				</SettingSection>
+				<SettingSection title={t((translations) => translations.settings.sections.globalVolume.title)}>
+					<SettingTitle />
+					<Setting
+						checked={settings.enable_remember_last_volume?.toString() !== "true" && settings.enable_global_volume?.toString() === "true"}
+						disabled={settings.enable_remember_last_volume?.toString() === "true"}
+						disabledReason={t((translations) => translations.pages.options.extras.optionDisabled.specificOption.globalVolume)}
+						label={t((translations) => translations.settings.sections.globalVolume.enable.label)}
+						onChange={setCheckboxOption("enable_global_volume")}
+						parentSetting={null}
+						title={t((translations) => translations.settings.sections.globalVolume.enable.title)}
+						type="checkbox"
+					/>
+					<Setting
+						disabled={settings.enable_remember_last_volume?.toString() === "true" || settings.enable_global_volume?.toString() !== "true"}
+						label={t((translations) => translations.settings.sections.globalVolume.settings.amount.label)}
+						max={100}
+						min={1}
+						onChange={setValueOption("global_volume")}
+						parentSetting={null}
+						title={t((translations) => translations.settings.sections.globalVolume.settings.amount.title)}
+						type="number"
+						value={settings.global_volume}
+					/>
 				</SettingSection>
 				<SettingSection title={t((translations) => translations.settings.sections.customCSS.title)}>
 					<SettingTitle />
