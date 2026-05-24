ca63b12ef126517d8bf7610064764ce473e87739
VampireChicken12 <Johnbrown51@yahoo.com>
2026-05-24 15:50:43 -0400

    fix(auto disable autoplay): restore previous state when disabling the feature

--- src/features/automaticallyDisableAutoPlay/index.ts
+++ src/features/automaticallyDisableAutoPlay/index.ts
@@ -1,9 +1,10 @@
-import type { YouTubePlayerDiv } from "@/src/types";
+import type { Nullable, YouTubePlayerDiv } from "@/src/types";
 
 import { createFeature } from "@/src/features/_registry/createFeature";
 import { waitForElement } from "@/src/utils/dom/wait";
 
 import { metadata } from "./index.metadata";
+let previousAutoPlayState: Nullable<boolean> = null;
 
 export default createFeature({
 	...metadata,
@@ -14,9 +15,11 @@ export default createFeature({
 		if (!autoPlayButtonElem) return;
 		const autoPlayButtonElemChecked = autoPlayButtonElem.querySelector(".ytp-autonav-toggle-button");
 		if (!autoPlayButtonElemChecked) return;
-		const isAutoPlayOn = autoPlayButtonElemChecked.getAttribute("aria-checked") === "false";
-		if (!isAutoPlayOn) return;
-		autoPlayButtonElem.click();
+		if (previousAutoPlayState === null) return;
+		const current = autoPlayButtonElemChecked.getAttribute("aria-checked") === "true";
+		if (current !== previousAutoPlayState) {
+			autoPlayButtonElem.click();
+		}
 	},
 	onEnable: async () => {
 		const playerContainer = await waitForElement<YouTubePlayerDiv>("div#movie_player", 1000);
@@ -25,8 +28,10 @@ export default createFeature({
 		if (!autoPlayButtonElem) return;
 		const autoPlayButtonElemChecked = autoPlayButtonElem.querySelector(".ytp-autonav-toggle-button");
 		if (!autoPlayButtonElemChecked) return;
-		const isAutoPlayOn = autoPlayButtonElemChecked.getAttribute("aria-checked") === "true";
-		if (!isAutoPlayOn) return;
-		autoPlayButtonElem.click();
+		const current = autoPlayButtonElemChecked.getAttribute("aria-checked") === "true";
+		previousAutoPlayState = current;
+		if (current) {
+			autoPlayButtonElem.click();
+		}
 	}
 });
