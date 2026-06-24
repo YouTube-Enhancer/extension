88a383454ce8db1dfdbe0b83f8ccc9961844a075
VampireChicken12 <Johnbrown51@yahoo.com>
2026-06-24 09:56:19 -0400

    refactor(automaticallyMaximizePlayer): migrate to executeWithRetries (#1337)

--- src/features/automaticallyMaximizePlayer/index.ts
+++ src/features/automaticallyMaximizePlayer/index.ts
@@ -1,14 +1,31 @@
 import { createFeature } from "@/src/features/_registry/createFeature";
+import { registry } from "@/src/features/_registry/featureRegistry";
 import { maximizePlayer, minimizePlayer } from "@/src/features/maximizePlayerButton/utils";
 
 import { metadata } from "./index.metadata";
 
+function makeMaximizeTask(): () => Promise<boolean> {
+	return async (): Promise<boolean> => {
+		await maximizePlayer(2000);
+		return document.body.getAttribute("yte-maximized") === "";
+	};
+}
+
 export default createFeature({
 	...metadata,
 	onDisable: () => minimizePlayer(),
-	onEnable: () => maximizePlayer(15000),
-	onNavigate: async () => {
-		await minimizePlayer();
-		await maximizePlayer(15000);
+	onEnable: () => {
+		void registry.playerManager.executeWithRetries("automaticallyMaximizePlayer", [makeMaximizeTask()], ["maximize"], {
+			maxAttempts: 15,
+			overallTimeout: 20000,
+			waitForLoaded: true
+		});
+	},
+	onNavigate: () => {
+		void registry.playerManager.executeWithRetries("automaticallyMaximizePlayer", [makeMaximizeTask()], ["maximize"], {
+			maxAttempts: 15,
+			overallTimeout: 20000,
+			waitForLoaded: true
+		});
 	}
 });
